284d93e235282c563741308326b72fdaacb4190a
David Gimeno Mañé <DGM@Mac-Pro-de-David.local>
2016-09-08 15:54:27 +0200

    petitions search working

--- js/content_controller.js
+++ js/content_controller.js
@@ -1,69 +1,31 @@
-// petitioncontroller.js
+//content_controller.js
 
-var spotifyApi = new SpotifyWebApi();
+searchPetition();
 
-var numSongs = 10;
-var numArtists = 6;
-var numAlbums = 6;
+function searchPetition(){
 
-
-//Funció que obté les cançons demanades per l'usuari i les afegeix a la llista de cançons obtingudes
-function getSongsObject(petitionName, numSongs){	
+	$(document).ready(function(){
 		
-	var songsObject = new Array();
+		/*var songData = {customer[
+			{ 	song: "hola",
+				album: "hola",
+				album_image: "hola",
+				artist: "hola",
+				audio_preview: "hola"
+			}			
+		]};
+		var data = new Array();
+		data = getSongsObject("ac");
+		console.log(data);
 
-	spotifyApi.searchTracks(petitionName, {limit: numSongs}).then(function(data) {    
-	    console.log(data);
-	    for (var i = 0; i < numSongs; i++) {
-	    	
-	    	var songInfo = { song: data.tracks.items[i].name,
-	    				album: data.tracks.items[i].album.name, 
-	    				album_image: data.tracks.items[i].album.images[1].url,
-	    				artist: data.tracks.items[i].artists[0].name,
-	    				audio_preview: data.tracks.items[i].preview_url
-	    			};
-	    			
-	    	songsObject.push(songInfo);
-	    }	
-	}, function(err) {
-		console.error(err);
+		var source = $('#song-template').html();
+		var template = Handlebars.compile(source);
+		console.log(template(data));
+
+		$('#songs-container').append(template(data));*/
+
+
+	
 	});
-
-	//console.log(songsObject);
-	return songsObject;
-
 }
 
-//Funció que obté els àlbums demanats per l'usuari i els afegeix a la llista d'àlbums obtingudes
-function getAlbums(petitionName, numAlbums){	
-		
-		$("#albumsList").empty();
-
-		spotifyApi.searchAlbums(petitionName, {limit: numAlbums}).then(function(data) {        
-		    for (var i = 0; i < numAlbums; i++) {
-		    	 $("#albumTemplate").tmpl(data.albums.items[i]).appendTo("#albumsList");
-		    }   	
-		  }, function(err) {
-		    console.error(err);
-		  });
-
-}
-
-//Funció que obté els artistes demanats per l'usuari i els afegeix a la llista d'artistes obtingudes
-function getArtists(petitionName, numArtists){	
-		
-		$("#artistsList").empty();
-
-		spotifyApi.searchArtists(petitionName, {limit: numArtists}).then(function(data) {    
-		    for (var i = 0; i < numArtists; i++) {
-		    	 $("#artistTemplate").tmpl(data.artists.items[i]).appendTo("#artistsList");
-		    }   	
-		  }, function(err) {
-		    console.error(err);
-		  });
-
-}
-
-var so = new Array();
-so = getSongsObject("ac", numSongs);
-console.log(so);
--- js/petitions_controller.js
+++ js/petitions_controller.js
@@ -2,35 +2,38 @@
 
 var spotifyApi = new SpotifyWebApi();
 var constants = Object.freeze({
-    num_songs: 10,
+    num_songs: 8,
     num_albums: 6,
-    num_artists: 6
+    num_artists: 5
 });
 
+getSongsObject("ac");
+getAlbumsObject("ac");
+getArtistsObject("ac");
+
 function getSongsObject(petitionName){	
-		
-	var songsObject = new Array();
 
 	spotifyApi.searchTracks(petitionName, {limit: constants.num_songs}).then(function(data) {    
 
+		var source = $("#song-template").html();
+		var template = Handlebars.compile(source);		
+
 	    for (var i = 0; i < constants.num_songs; i++) {
-	    	
+
 	    	var songInfo = { song: data.tracks.items[i].name,
 	    				album: data.tracks.items[i].album.name, 
 	    				album_image: data.tracks.items[i].album.images[1].url,
 	    				artist: data.tracks.items[i].artists[0].name,
 	    				audio_preview: data.tracks.items[i].preview_url
-			};			
-	    	songsObject.push(songInfo);
+			};
+
+			var html = template(songInfo);
+			$('#songs-container').append(html);	    	
 	    }	
 
 	}, function(err) {
 		console.error(err);
 	});
-
-	console.log(songsObject);
-	return songsObject;
-
 }
 
 function getAlbumsObject(petitionName){	
@@ -38,43 +41,44 @@ function getAlbumsObject(petitionName){
 	var albumsObject = new Array();
 
 	spotifyApi.searchAlbums(petitionName, {limit: constants.num_albums}).then(function(data) {        
+		console.log(data);
+		var source = $("#albums-template").html();
+		var template = Handlebars.compile(source);	
 
 	    for (var i = 0; i < constants.num_albums; i++) {
 	    	var albumInfo = { album_image: data.albums.items[i].images[1].url,
 	    				name: data.albums.items[i].name, 
 	    				spotify_link: data.albums.items[i].external_urls.spotify
 			};			
-	    	albumsObject.push(albumInfo);	    	
+			
+			var html = template(albumInfo);
+			$('#albums-container').append(html);	    	
 		}   	
 	}, function(err) {
 		console.error(err);
 	});
-
-	//console.log(albumsObject);
-	return albumsObject;		
-
 }
 
 function getArtistsObject(petitionName){	
 
-	var artistsObject = new Array();
-
 	spotifyApi.searchArtists(petitionName, {limit: constants.num_artists}).then(function(data) {    
+
+		var source = $("#artists-template").html();
+		var template = Handlebars.compile(source);
 
 	    for (var i = 0; i < constants.num_artists; i++) {
     		var artistInfo = { name: data.artists.items[i].name,
     				popularity: data.artists.items[i].popularity, 
     				num_followers: data.artists.items[i].followers.total,
     				spotify_link: data.artists.items[i].external_urls.spotify,
-    				artist_image: data.artists.items[i].images[2].url
-			};			
-    		artistsObject.push(artistInfo);
+    				artist_image: data.artists.items[i].images[2].url,
+    				genres: data.artists.items[i].genres
+			};	
+			
+			var html = template(artistInfo);
+			$('#artists-container').append(html);
 	    }   	
 	}, function(err) {
 		console.error(err);
 	});
-
-	//console.log(artistsObject);
-	return artistsObject;
-
 }
